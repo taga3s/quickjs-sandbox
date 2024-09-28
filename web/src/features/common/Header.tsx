25abf4c05e3154f8417970c9a272a9643f795eda
taga3s <bepozurapu274@gmail.com>
2024-09-29 00:22:25 +0900

    fix: page title

--- web/src/features/common/Header.tsx
+++ web/src/features/common/Header.tsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa";
 const Header: FC = () => {
 	return (
 		<header className={headerStyle.wrapper}>
-			<h1 className={headerStyle.title}>taga3s; js_playground</h1>
+			<h1 className={headerStyle.title}>js_playground</h1>
 			<a href="https://github.com/taga3s/js-playground">
 				<FaGithub color="black" size={32} />
 			</a>
