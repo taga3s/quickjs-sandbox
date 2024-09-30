318ed40e17291691e3e11b878521f4a3af0d4f30
taga3s <bepozurapu274@gmail.com>
2024-09-30 09:55:42 +0900

    update css styles

--- web/src/features/common/Header.tsx
+++ web/src/features/common/Header.tsx
@@ -1,11 +1,14 @@
 import type { FC } from "preact/compat";
 import headerStyle from "./Header.module.css";
+import { GitHubIcon } from "./icons/Github";
 
 const Header: FC = () => {
 	return (
 		<header className={headerStyle.wrapper}>
 			<h1 className={headerStyle.title}>js_playground</h1>
-			<a href="https://github.com/taga3s/js-playground">Github</a>
+			<a className={headerStyle.githubIcon} href="https://github.com/taga3s/js-playground">
+				<GitHubIcon />
+			</a>
 		</header>
 	);
 };
