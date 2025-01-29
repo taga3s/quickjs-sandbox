895363405a913f2c137a33fdb458c6df7645f182
taga3s <bepozurapu274@gmail.com>
2025-01-29 21:38:44 +0900

    remove /server

--- server/docker/db/script/init.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists js_code (
-    id serial primary key auto_increment,
-    code text not null,
-    hashed_code text not null,
-    created_at timestamp not null default current_timestamp
-);
--- server/src/handlers/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod collect;
-pub mod healthz;
-pub mod share;
